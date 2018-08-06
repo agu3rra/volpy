85fe97eca91c0effb53513b774e1426542e9a57b
Andre Guerra <agu3rra@Andres-MacBook-Pro.local>
2018-08-05 22:44:07 -0300

    Coded 2 Unittest cases. Renamed datasets directory

--- volpy/test/test_gps_survey.py
+++ volpy/test/test_gps_survey.py
@@ -1,10 +1,24 @@
 import unittest
+import sys
+import os
+sys.path.append('../lib')
+
+from gps_survey import GpsDevice
+from gps_survey import GpsSurvey
 
 class GpsSurveyTestCase(unittest.TestCase):
 
 
     def test_ImportCleanData(self):
-        pass
+        SAMPLE_DATA = '../sample_data/survey_ibema_faxinal.gpx'
+        sample_survey = GpsSurvey("Sample initialization", SAMPLE_DATA)
+        self.assertEqual(sample_survey.data.shape[0], 155)
 
     def test_ImportDirtyData(self):
-        pass
+        SAMPLE_DATA = '../sample_data/survey_ibema_faxinal_dirty.gpx'
+        sample_survey = GpsSurvey("Sample initialization", SAMPLE_DATA)
+        # As we know only 152 points are valid
+        self.assertEqual(sample_survey.data.shape[0], 152)
+
+if __name__ == '__main__':
+    unittest.main()
